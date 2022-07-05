1937e7a6516b8b8111851b54ae3893da8027bdae
Yiyi Wang <eewang1145@gmail.com>
2022-07-05 05:01:34 +0000

    Finished contact-me lab

--- lab18/contact_me/routes/contacts.js
+++ lab18/contact_me/routes/contacts.js
@@ -1,7 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/client')
 
 router.get('/', (req, res) => {
-    res.send(`Your are login as: ${res.locals.user}`);
+    knex('contacts').then(list => {
+        if (list) {
+            marketing = list.filter(contact => contact.department == 'Marketing');
+            sales = list.filter(contact => contact.department == 'Sales');
+            technical = list.filter(contact => contact.department == 'Technical');
+            res.render('contacts/all', { marketing: marketing, sales: sales, technical: technical, contact: false })
+        } else {
+            res.send("Cannot find the contact list");
+        }
+    })
 })
+
+router.post('/', (req, res) => {
+    knex('contacts').insert({
+        name: req.body.contactName,
+        address: req.body.address,
+        department: req.body.department,
+        message: req.body.message
+    }).returning('*').then(() => {
+        res.redirect('/contacts');
+    })
+})
+
+router.get('/:id/edit', (req, res) => {
+    const id = req.params.id;
+    let marketing, sales, technical;
+    knex('contacts').then(list => {
+        if (list) {
+            marketing = list.filter(contact => contact.department == 'Marketing');
+            sales = list.filter(contact => contact.department == 'Sales');
+            technical = list.filter(contact => contact.department == 'Technical');
+            return knex('contacts').where('id', id).first();
+        }
+    }).then(contact => {
+        res.render('contacts/all', { marketing: marketing, sales: sales, technical: technical, contact: contact })
+    })
+})
+
+router.patch('/:id', (req, res) => {
+    const id = req.params.id;
+    knex('contacts').where('id', id).first().update({
+        name: req.body.contactName,
+        address: req.body.address,
+        department: req.body.department,
+        message: req.body.message
+    }).then(() => {
+        res.redirect('/contacts');
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    knex('contacts').where('id', id).first().del().then(() => {
+        res.redirect('/contacts');
+    })
+})
+
 module.exports = router;
